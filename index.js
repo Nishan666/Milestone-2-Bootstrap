2d1856226256b9234e1525dfc59771710821bae2
nishan666 <nishankumar559@gmail.com>
2024-02-23 15:30:17 +0530

    Improved visual consistency with refined alignment, sizing, and corresponding code enhancements.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openSidebar() {
-    document.getElementById("sidebar").style.width = "50%";
-    document.getElementById("page").style.opacity = "0.5";
+    document.getElementById("sidebar").style.width = "40%";
+    document.getElementById("page").style.opacity = ".99";
     document.getElementById("overlay").style.display = "block";
 
 }
@@ -12,10 +12,8 @@ function closeSidebar() {
 }
 
 document.getElementById("btnOpenSidebarCreate").addEventListener("click", openSidebar);
-document.getElementById("btnOpenSidebarViewMore").addEventListener("click", openSidebar);
-document.getElementById("btnOpenSidebarEdit").addEventListener("click", openSidebar);
-document.getElementById("btnOpenSidebarView").addEventListener("click", openSidebar);
-document.getElementById("btnOpenSidebarCreateEmpty").addEventListener("click", openSidebar);
+document.getElementById("btnOpenSidebarCreateEmpty").addEventListener("click", openSidebar);    
+
 
 function filledOffers() {
     document.getElementById("withOffer").style.display = "block"
@@ -29,6 +27,40 @@ function emptyOffers() {
 }
 
 
+
+////////////////
+
+function setupSidebarButtons() {
+    for (let i = 1; i <= 4; i++) {
+        setupSidebarButtonListeners(`btnOpenSidebarViewMore${i}`);
+        setupSidebarButtonListeners(`btnOpenSidebarEdit${i}`);
+        setupSidebarButtonListeners(`btnOpenSidebarView${i}`);
+    }
+}
+
+function setupSidebarButtonListeners(buttonId) {
+    document.getElementById(buttonId).addEventListener("click", openSidebar);
+}
+
+setupSidebarButtons();
+
+// or
+
+// document.getElementById("btnOpenSidebarViewMore1").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarEdit1").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarView1").addEventListener("click", openSidebar);
+
+// document.getElementById("btnOpenSidebarViewMore2").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarEdit2").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarView2").addEventListener("click", openSidebar);
+
+// document.getElementById("btnOpenSidebarViewMore3").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarEdit3").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarView3").addEventListener("click", openSidebar);
+
+// document.getElementById("btnOpenSidebarViewMore4").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarEdit4").addEventListener("click", openSidebar);
+// document.getElementById("btnOpenSidebarView4").addEventListener("click", openSidebar);
 
 
 
